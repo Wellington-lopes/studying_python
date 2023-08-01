c36ff4db65c0c429a084b2386020428cd6001145
Wellington-lopes <wsoaresllopes@gmail.com>
2023-08-01 14:31:45 -0300

    treinando for

---
+++ alunonotas.py
@@ -0,0 +1,11 @@
+soma = 0
+
+for i in range(1,3):
+    nota = float(input(f'Digite as 2 notas que o aluno recebeu {i}: '))
+    soma = soma + nota
+
+print('A média do aluno é ', soma / 2 )
+
+"""nota1 = float(input('Digite a nota 1 do aluno: ' ))
+
+print('A nota do aluno é ', nota1)"""
